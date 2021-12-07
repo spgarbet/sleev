b46c0b45a0c9d0c11e22f8e6bca062f73727ea88
Joey Sherrill <joeysherrill1387@gmail.com>
2021-12-07 17:20:22 -0600

    Update package name

---
+++ R/sleev-package.R
@@ -0,0 +1,10 @@
+## usethis namespace: start
+#' sleev: combining functions from logreg2ph and TwoPhaseReg
+#'
+#'
+#' @name sleev
+#'
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib sleev, .registration=TRUE
+## usethis namespace: end
+NULL
