55cd073aacf1d8e39d0060930466875abd655302
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-03-08 15:23:37 -0500

    Make gamma_pred and Y_unval optional arguments in pl_theta()

--- R/pl_theta.R
+++ R/pl_theta.R
@@ -25,8 +25,8 @@
 #' @return Profile likelihood for `theta` after perturbing element `k` by `h_N`.
 #' @noRd
 
-pl_theta <- function(k, theta, h_N, n, N, Y_unval, Y, X_unval, X, Z, Bspline, comp_dat_all,
-                        theta_pred, gamma_pred, gamma0 = NULL, p0 = NULL, p_val_num = NULL, TOL, MAX_ITER) {
+pl_theta <- function(k, theta, h_N, n, N, Y_unval = NULL, Y, X_unval, X, Z, Bspline, comp_dat_all,
+                        theta_pred, gamma_pred = NULL, gamma0 = NULL, p0 = NULL, p_val_num = NULL, TOL, MAX_ITER) {
   pert <- theta
   pert[k] <- pert[k] + h_N
   pl_params <- profile_out(theta = pert,
